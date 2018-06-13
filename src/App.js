d1c0cbedfbd01b35714bfc0bc991700c9e1d8307
Hector <hjarte1@hotmail.com>
2018-06-13 19:08:37 -0400

    Added visual feedback for  sorting options of the search bar, set the state of sorting options and input elements, simulated a search query

--- src/App.js
+++ src/App.js
@@ -26,6 +26,9 @@ const businesses = [
 ];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy){
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
   render() {
     return (
       <div className="App">
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,15 +8,62 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSortByChange = this.handleSortByChange.bind(this);
+
+
+  }
+
+  //Checks the state of sortBy to change the css to give feedback to ther user
+  getSortByClass(sortByOption){
+    if(this.state.sortBy === sortByOption){
+      return 'active';
+    }
+    else {
+      return '';
+    }
+  }
+
+  handleSortByChange(sortByOption){
+    this.setState({sortBy: sortByOption});
+  }
+  handleTermChange(event){
+    this.setState({term: event.target.value})
+  }
+  handleLocationChange(event){
+    this.setState({location: event.target.value})
+  }
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+
+    event.preventDefault();
+  }
+
   //Dynamically create the list items needed to display the sort options
   renderSortByOptions(){
     //Access the keys of the sortByOptions object and iterate through them using the map() method
     return Object.keys(sortByOptions).map(sortByOption => {
       //Access the sortByOptions values using the sortByOption parameter of the callback function and stores them in sortByOptionValue
-      let sortByOptionValue = sortByOption[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return (<li className={this.getSortByClass(sortByOptionValue)}
+                  key={sortByOptionValue}
+                  onClick={this.handleSortByChange}>
+                {sortByOption}
+             </li>);
     });
   }
+
   render(){
     //Sets the way SearchBar will be displayed
     return (
@@ -28,11 +75,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+          <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
-          <a>Lets Go</a>
+          <a onClick={this.handleSearch}>Lets Go</a>
         </div>
       </div>
 
